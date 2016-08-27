e2f0f5259e3a5be4b5f225aea90cf991909e4ba7
Tushar Shah <MaC@Tushars-MBP-2.fios-router.home>
2016-08-27 15:17:56 -0400

    matrix

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -90,3 +90,124 @@ Determinates only exist for square matrices
 
 
 print alpha * u
+
+
+Matrix Operations
+
+3.1)DOE different dimensions
+
+3.2 (A - C^T)
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+
+#print 
+print a - c.getT()
+
+3.3)
+
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+print c.getT() + 3 * d
+
+3.4)  BA
+
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+
+#print BA
+print b * a 
+
+3.5) BA^T
+
+After transpose incompatable dimensions
+
+3.6 ) Different Dimmensions
+
+3.7)
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+
+#CB
+print c * b
+
+3.8)B^4
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+print b * b * b * b
+
+
+3.9)AA^T
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+print a * a.getT()
+
+
+3.10)D^TD
+import numpy as np
+
+a = np.matrix("1 2 3; 2 7 4")
+b = np.matrix("1 -1; 0 1")
+c = np.matrix("5 -1; 9 1; 6 0")
+d = np.matrix("3 -2 -1; 1 2 3")
+u = np.matrix("6 2 -3 5")
+v = np.matrix("3 5 -1 4")
+w = np.matrix("1; 8; 0; 5")
+alpha = int(6)
+
+
+
+print d.getT() * d
